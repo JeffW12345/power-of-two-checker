e4af7c5f6be2c05df44650d642038c21ea004453
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2021-09-02 13:40:50 +0100

    Update PowerOfTwo.java
    Improved comments.

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -21,6 +21,10 @@ public class PowerOfTwo {
 		// if 2^x = num, Math.log(num) / Math.log(2) returns the value of x
 		// as per https://stackoverflow.com/questions/3305059/how-do-you-calculate-log-base-2-in-java-for-integers
 		double exponentAsInt = (int) (Math.log(num) / Math.log(2)); // Zeros everything after the decimal point.
+		// Does 2 to the power of 'exponentAsInt' equal 'num'? If so, 'num' fulfils the criteria for being a power of 2.
+		// This check avoids the problem of potential problems arising from floating point number errors if 
+		// (int) (Math.log(num) / Math.log(2) == Math.log(num) / Math.log(2)
+		// had been used to check if the expononent is an integer. 
 		if (Math.pow(2, exponentAsInt) == num && num > 0) {
 			return true;
 		}
