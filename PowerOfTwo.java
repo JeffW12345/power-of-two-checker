196d4147b9a34f1a80dc2ac2a301d296e5868b5f
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2021-10-09 23:53:24 +0100

    Update PowerOfTwo.java
    Simpler solution.

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -8,27 +8,15 @@
 public class PowerOfTwo {
 
 	private static void printPOWERIfPowerOfTwoAndIntIfNot(int maxNum) {
+		int next = 1;
 		for (int i = 0; i < maxNum + 1; i++) {
-			if (isPowerOfTwo(i)) {
+			if (i == next) {
 				System.out.println("POWER");
+				next *= 2;
 			} else {
 				System.out.println(i);
 			}
 		}
-	}
-
-	private static boolean isPowerOfTwo(int num) {
-		// if 2^x = num, Math.log(num) / Math.log(2) returns the value of x
-		// as per https://stackoverflow.com/questions/3305059/how-do-you-calculate-log-base-2-in-java-for-integers
-		double exponentAsInt = (int) (Math.log(num) / Math.log(2)); // Zeros everything after the decimal point.
-		// Does 2 to the power of 'exponentAsInt' equal 'num'? If so, 'num' fulfils the criteria for being a power of 2.
-		// This check avoids potential problems arising from floating point number errors if 
-		// (int) (Math.log(num) / Math.log(2)) == Math.log(num) / Math.log(2)
-		// had been used to check if the exponent is an integer. 
-		if (Math.pow(2, exponentAsInt) == num && num > 0) {
-			return true;
-		}
-		return false;
 	}
 
 	public static void main(String[] args) {
